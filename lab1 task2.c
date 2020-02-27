6c7e6fae1ace32f459369d1cf2a9d553d6779bf9
Johnson Pimkot <c3303127@uon.edu.au>
2020-02-27 16:35:16 +1100

    initial commit

---
+++ lab1 task2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int k;
+    printf("Enter an Integer:");
+    scanf("%d", &k);
+    k=k*2;
+    printf("The integer double is: %d\n", k);
+    return 0;
+}
+
+
